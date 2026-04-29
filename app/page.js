32909ac86ef113896d3ee9c5c0a91765910cac72
Umer Ijaz <umerijaaz@protonmail.com>
2026-04-29 15:19:40 +0200

    daisyui component library installed and styling added

--- app/page.js
+++ app/page.js
@@ -1,15 +1,19 @@
 import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
-  
+
   const isLoggedIn= true;
   const name= "Umer";
   return (
     <main>
-      <h1>Kundenfeedback sammeln, bessere Produkte bauen</h1>
-      <div>Feedback-Boards in Minuten erstellen, Features gezielt priorisieren und Produkte entwickeln, 
+      <section className= "px-8 text-center py-32 max-w-3xl mx-auto">
+        <h1 className= "text-4xl font-extrabold mb-6">Kundenfeedback sammeln, 
+        bessere Produkte bauen</h1>
+        <div className="opacity-90 mb-10">Feedback-Boards in Minuten erstellen, 
+        Features gezielt priorisieren und Produkte entwickeln, 
         die deine Kunden lieben.</div>
         <ButtonLogin isLoggedIn={isLoggedIn} name={name}/>
+      </section>
     </main>
   );
 }
--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const ButtonLogin = ({isLoggedIn, name})=> {
     if (isLoggedIn)
     return (
-    <Link href= "/dashboard"> Welcome Back, {name} </Link>
+    <Link href= "/dashboard" className="btn btn-primary"> Welcome Back, {name} </Link>
     )
     else {
         return <button>Log in</button>
