8e7f9fbbce089cf31ba3b6db637967a6fdda82b4
Umer Ijaz <umerijaaz@protonmail.com>
2026-04-30 14:50:59 +0200

    pricing section added

--- app/page.js
+++ app/page.js
@@ -1,30 +1,58 @@
 import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
-
-  const isLoggedIn= true;
-  const name= "Umer";
+  const isLoggedIn = true;
+  const name = "Umer";
   return (
     <main>
+      {/*HEADER*/}
       <section className="bg-base-200">
-      <div className="flex justify-between items-center px-8 py-2 max-w-3xl mx-auto">
-        <div className="font-bold">FeedbackFast</div>
-        <div className="space-x-4 max-md:hidden">
-          <a className="link link-hover">Preis</a>
-          <a className="link link-hover">FAQ</a>
+        <div className="flex justify-between items-center px-8 py-2 max-w-3xl mx-auto">
+          <div className="font-bold">FeedbackFast</div>
+          <div className="space-x-4 max-md:hidden">
+            <a className="link link-hover">Preis</a>
+            <a className="link link-hover">FAQ</a>
+          </div>
+          <div>
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+          </div>
         </div>
-        <div>
-          <ButtonLogin isLoggedIn={isLoggedIn} name={name}/>
-        </div>
-      </div>
       </section>
-      <section className= "px-8 text-center py-32 max-w-3xl mx-auto">
-        <h1 className= "text-4xl font-extrabold mb-6 lg:text-5xl">Kundenfeedback sammeln, 
-        bessere Produkte bauen</h1>
-        <div className="opacity-90 mb-10">Feedback-Boards in Minuten erstellen, 
-        Features gezielt priorisieren und Produkte entwickeln, 
-        die deine Kunden lieben.</div>
-        <ButtonLogin isLoggedIn={isLoggedIn} name={name}/>
+      {/*HERO*/}
+      <section className="px-8 text-center py-32 max-w-3xl mx-auto">
+        <h1 className="text-4xl font-extrabold mb-6 lg:text-5xl">
+          Kundenfeedback sammeln, bessere Produkte bauen
+        </h1>
+        <div className="opacity-90 mb-10">
+          Feedback-Boards in Minuten erstellen, Features gezielt priorisieren
+          und Produkte entwickeln, die deine Kunden lieben.
+        </div>
+        <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
+      {/* PRICING */}
+      <section className="bg-base-200">
+        <div className="px-8 py-32 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-primary text-center">
+            Preise
+          </p>
+          <h2 className="text-3xl font-extrabold mb-12 lg:text-4xl text-center">
+            Preise, die sich deinen Bedürfnissen anpassen
+          </h2>
+          <div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto">
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl font-black">19€</div>
+              <div className="uppercase text-sm font-medium opacity-60">
+                /monat
+              </div>
+            </div>
+            <ul>
+              <li>Kundenfeedback sammeln</li>
+              <li>Unbegrenzte Boards</li>
+              <li>Admin-Dashboard</li>
+              <li>24/7 Support</li>
+            </ul>
+          </div>
+        </div>
       </section>
     </main>
   );
